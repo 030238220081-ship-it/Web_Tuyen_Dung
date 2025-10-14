d5101db92f35541e2498f9dd05a07256491d9053
Quoc <hungqn24104@gmail.com>
2025-10-14 19:30:35 +0700

    Fix root URL configuration to resolve 404

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('recruitment.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('recruitment.urls')),
 ]
 
 if settings.DEBUG:
