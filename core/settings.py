872646228396ae35b2d0939a4ba4669c69b09600
Quoc <hungqn24104@gmail.com>
2025-10-14 21:44:23 +0700

    Fix: Add STATICFILES_STORAGE for whitenoise

--- core/settings.py
+++ core/settings.py
@@ -95,6 +95,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # --- Cấu hình khác (Đã đúng) ---
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'recruitment.CustomUser'
